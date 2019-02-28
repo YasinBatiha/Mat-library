57e57936b99be077627340e970c27fd9fccd9ea6
Yasin Batiha <47186696+YasinBatiha@users.noreply.github.com>
2019-02-28 20:54:20 +0200

    final

--- Assigment_2/Task_1/test_trapezoidal12.m
+++ Assigment_2/Task_1/test_trapezoidal12.m
@@ -1,11 +1,11 @@
 %% Test trapezoidal lin. function
-a=1.2;
-b=4.4;
-n=2;
-F=@(x)3*1e8*x.^2-4*1e6*x;
-f=@(x)6*1e8*x-4*1e6;
-computed=Int_trapziodal(f,a,b,n);
+a=1.2;  % left border of integration interval
+b=4.4;  % right ...
+n=1;    % number of elements
+F=@(x)3*1e8*x.^2-4*1e6*x;   % Indefinite integral of testing function
+f=@(x)6*1e8*x-4*1e6;    % testing function
+computed=Int_trapziodal(f,a,b,n);   
 expected=F(b)-F(a);
-diff=eps(expected)*1e+2;
-err=abs(expected-computed);
-assert(err<diff, computed);
+tol=eps(expected)*1e+2;    % tolerance
+err=abs(expected-computed); % difference
+assert(err<tol, computed);
